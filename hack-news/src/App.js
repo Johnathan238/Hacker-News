a0932e792a97cb08ba8a9edcbe4a07820547075d
Johnathan <jonathanumana86@gmail.com>
2021-02-26 19:17:40 -0500

    Making the Routes in App

--- hack-news/src/App.js
+++ hack-news/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import HacknewsList from './containers/HacknewsList'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route path={"/"} exact component={HacknewsList}/>
+        <Route path={"/hacknews/:hacknews"} exact component={HacknewsList}/>
+        <Redirect to={"/"} />
+      </Switch>
     </div>
   );
 }
---
+++ hack-news/src/containers/HacknewsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HacknewsList = () => {
+    return (
+        <div>
+            <h1>WHATSNEW!!!</h1>
+        </div>
+    );
+}
+
+export default HacknewsList
