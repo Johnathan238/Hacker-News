a93192658fd0f867f3a039722c08440965d2cbf9
Johnathan <jonathanumana86@gmail.com>
2021-03-02 20:31:48 -0500

    I'm understanding more redux and I'm getting better, redid actions

--- hack-news/src/actions/NewsActions.jsx
+++ hack-news/src/actions/NewsActions.jsx
@@ -1,22 +1,51 @@
 import axios from  'axios';
+import { stories_API, search_API } from '../containers/API'
+
+
+
 
 export const GetNewsList = () => async dispatch => {
-    try {
-        dispatch({
-            type: "NEWS_LIST_LOADING"
-        });
-        
-        const perPage = 20;
-        const res = await axios.get(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`)
 
-        dispatch({
-            type: "NEWS_LIST_SUCCESS",
-            payload: res.data
-        })
-    } catch (e) {
-        dispatch({
-            type: "NEWS_LIST_FAILED",
-        })
-    }
+    const res = await stories_API.get('')
+        
+    dispatch({
+        type: "NEWS_LIST_LOADING",
+        payload: res.data.hits
+    })
+
 };
+
+export const search_API = () => async dispatch => {
+    const res = await search_API.get(`${query}`)
+        
+    dispatch({
+        type: "GOT_NEWS",
+        payload: res.data.hits
+        });
+
+    dispatch({
+        type: "CLEAR_NEWS",
+        payload: query
+    })
+};
+
+
+    // try {
+    //     dispatch({
+    //         type: "LIST_LOADING"
+    //     });
+        
+    //     const perPage = 20;
+    //     const res = await axios.get(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`)
+
+    //     dispatch({
+    //         type: "NEWS_LIST_SUCCESS",
+    //         payload: res.data
+    //     })
+    // } catch (e) {
+    //     dispatch({
+    //         type: "NEWS_LIST_FAILED",
+    //     })
+    // }
+
 
